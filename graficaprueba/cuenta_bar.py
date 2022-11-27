2db06d61c531fe9537fcd7205879f8edea046ae0
victorncn <vc33227@gmail.com>
2022-11-27 03:40:34 -0300

    nueva carpeta para pruebas con tkinter

---
+++ graficaprueba/cuenta_bar.py
@@ -0,0 +1,29 @@
+class Bar :
+    def __init__(self):
+        self.total=0
+        self.objetos=[]
+
+    menu= {'vino':90,
+        'milanesa':200,
+        'cocacola':75,
+        'carne':300,
+        'pollo':189,
+        'helado':140}
+
+    def add (self,objeto):
+        self.objetos.append(objeto)
+        self.total += self.menu[objeto]
+    
+    def factura (self,impuesto,servicio):
+        impuesto = (impuesto/100)*self.total
+        servicio = (servicio/100)*self.total
+        total=self.total+impuesto+servicio
+
+        for objeto in self.objetos:
+            print(objeto,'€',self.menu[objeto])
+        print('el total es: ','€',round(total,2))
+
+mesa1= Bar()
+mesa1.add('vino')
+mesa1.add('carne')
+mesa1.factura(5,6)
---
+++ graficaprueba/ventana.py
@@ -0,0 +1,15 @@
+from tkinter import*
+
+#creacion de ventana
+root = Tk()
+#creacion de etiqueta 
+etiqueta = Label(root,text="hola mundo")
+
+etiqueta.place(x=500,y=200,width=70,height=100)
+
+#etiqueta.pack()
+
+
+root.mainloop()
+
+
