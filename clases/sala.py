42780d7291c615f089d5fb40409559f1e51c0b56
fedecruz1981 <fede.ariel.cruz@gmail.com>
2022-12-02 12:03:06 -0300

    faltan definir metodos y algunas clases mas

--- clases/sala.py
+++ clases/sala.py
@@ -40,9 +40,11 @@ class Butacas:
      
 
 class Sala:
-    def _init_(self,id=0,numero=0):
+    def _init_(self,id=0,numero=0,tipo="2D",asientos=0):
        self._id=id
        self._numero=numero
+       self._tipo=tipo
+       self._asientos=asientos
     
     @property
     def id(self):
@@ -58,7 +60,25 @@ class Sala:
     
     @numero.setter
     def id(self,numero):
-        self._numero=numero   
+        self._numero=numero
+        
+    @property
+    def tipo(self):
+        return self._tipo
+    
+    @tipo.setter
+    def tipo(self,tipo):
+        self._tipo=tipo
+        
+    @property
+    def asientos(self):
+        return self._asientos
+    
+    @asientos.setter
+    def asientos(self,asientos):
+        self.asientos=asientos
+    
+   
         
         
         
